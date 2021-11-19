a3b4c57909c5eb15b421e4e5b4a7dd5576a5d30c
HarisSaran <hsaran100@academic.rrc.ca>
2021-11-18 20:14:33 -0600

    added routes for tools and producer companies

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  # get 'tools/index'
+  # get 'tools/show'
+  # get 'producer_companies/index'
+  # get 'producer_companies/show'
+
+  resources :tools, only: %i[index show]
+  resources :producer_companies, only: %i[index show]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
