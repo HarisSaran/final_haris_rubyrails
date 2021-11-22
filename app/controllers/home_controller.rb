62609b940909011eca2850719b5758cc33b43d55
HarisSaran <hsaran100@academic.rrc.ca>
2021-11-22 09:53:12 -0600

    displaying top 3 producer companies on home page with bulma, also displaying all producer comapnies on idnex

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,11 @@ class HomeController < ApplicationController
     @tools = Tool.includes(:producer_company)
                  .order("price DESC")
                  .limit(3)
+    @producer_companies = ProducerCompany.select("producer_companies.*")
+                                         .select("COUNT(producer_companies.id) as tool_count")
+                                         .left_joins(:tools)
+                                         .group("producer_companies.id")
+                                         .order("tool_count DESC")
+                                         .limit(3)
   end
 end
--- app/controllers/producer_companies_controller.rb
+++ app/controllers/producer_companies_controller.rb
@@ -1,7 +1,13 @@
 class ProducerCompaniesController < ApplicationController
   def index
+    @producer_companies = ProducerCompany.select("producer_companies.*")
+                                         .select("COUNT(producer_companies.id) as tool_count")
+                                         .left_joins(:tools)
+                                         .group("producer_companies.id")
+                                         .order("tool_count DESC")
   end
 
   def show
+    @producer_company = ProducerCompany.find(params[:id])
   end
 end
