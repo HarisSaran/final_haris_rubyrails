0fe17a03aea6343d1fca7b3ad43c4a3901cccca2
HarisSaran <hsaran100@academic.rrc.ca>
2021-11-25 16:39:36 -0600

    used kaminari for pagination on tools page

--- app/controllers/tools_controller.rb
+++ app/controllers/tools_controller.rb
@@ -1,7 +1,9 @@
 class ToolsController < ApplicationController
   def index
     # @tools = Tool.includes(:producer_company).all
-    @tools  = Tool.paginate(page: params[:page], per_page: 2)
+
+    @tools  = Tool.page(params[:page])
+    # @tools  = Tool.paginate(page: params[:page], per_page: 2)  first gem
   end
 
   def show
