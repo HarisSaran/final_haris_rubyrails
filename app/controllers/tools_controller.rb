409df3272f3b183362f5006d033abec6bae0c338
HarisSaran <hsaran100@academic.rrc.ca>
2021-11-25 16:17:58 -0600

    added pagination for tools

--- app/controllers/tools_controller.rb
+++ app/controllers/tools_controller.rb
@@ -1,6 +1,7 @@
 class ToolsController < ApplicationController
   def index
-    @tools = Tool.includes(:producer_company).all
+    # @tools = Tool.includes(:producer_company).all
+    @tools  = Tool.paginate(page: params[:page], per_page: 2)
   end
 
   def show
