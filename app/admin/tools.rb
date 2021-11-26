d5b24955ff299ba425f20d228c8388215db6cc61
HarisSaran <hsaran100@academic.rrc.ca>
2021-11-25 20:56:53 -0600

    updated active admin to upload images, and display images

--- app/admin/tools.rb
+++ app/admin/tools.rb
@@ -1,11 +1,21 @@
 ActiveAdmin.register Tool do
-
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  # permit_params :name, :description, :price, :quantity, :producer_company_id
+  permit_params :name, :description, :price, :quantity, :producer_company_id, :image
+
+  form do |f|
+    f.semantic_errors # shows errors on :base
+    f.inputs          # builds an input field for every attribute
+    # lets us input images
+    f.inputs do
+      f.input :image, as: :file, hint: f.object.image.present? ? image_tag(f.object.image) : ""
+    end
+    f.actions # adds the 'Submit' and 'Cancel' buttons
+  end
+
   #
   # or
   #
@@ -14,5 +24,4 @@ ActiveAdmin.register Tool do
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
-  
 end
--- app/models/tool.rb
+++ app/models/tool.rb
@@ -1,4 +1,5 @@
 class Tool < ApplicationRecord
+  has_one_attached :image
   belongs_to :producer_company
   has_many :tool_types
   has_many :tools, through: :tool_types
