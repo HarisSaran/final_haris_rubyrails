21807ba4ba53fc4fba126f7cda51a5e8a57fbd40
HarisSaran <hsaran100@academic.rrc.ca>
2021-11-30 13:11:54 -0600

    added shopping cart controller, display shopping cart in application, update routs, tool and index to take in item id for add and delete

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,12 +1,31 @@
 class CartController < ApplicationController
   # POST /cart
   def create
+    # product_id = params[:id]
+    # logger.debug("Adding #{product_id} to cart.")
+    # tool = Tool.find(product_id)
+    # session[:shopping_cart] >> product_id
+    # redirect_to root_path
+
     logger.debug("Adding #{params[:id]} to cart.")
+    id = params[:id].to_i
+    session[:shopping_cart] << id
+
+    tool = Tool.find(id)
+    flash[:notice] = "#{tool.name} was succesfully added to cart! Cheers 🍻"
+
+    redirect_to tools_path
   end
 
   # DELETE /cart/:id
   def destroy
     # removes params[:id]
+    logger.debug("Removing #{params[:id]} to cart.")
+    id = params[:id].to_i
+    session[:shopping_cart].delete(id)
+    tool = Tool.find(id)
+    flash[:notice] = " #{tool.name} removed from cart."
+    redirect_to tools_path
   end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -14,4 +14,6 @@ Rails.application.routes.draw do
   resources :producer_companies, only: %i[index show]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :cart, only: %i[create destroy]
 end
